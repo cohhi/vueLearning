ce581a08282164fa258ca92e1bcdfe8754ce7610
2507138658@qq.com <71576964+steam-404@users.noreply.github.com>
2023-04-09 12:28:13 +0800

    Update
    create 指令 and 模板语法
    
    Co-Authored-By: steam-404 <71576964+steam-404@users.noreply.github.com>

---
+++ Code/指令.vue
@@ -0,0 +1,27 @@
+<script>
+export default ({
+    data(){
+        return{
+            message1:"纯文本数据",
+            message2:"<span style='color: red'>采用html语法填充</span>",
+            message3:"{{}}语法，插值表达式",
+            message4:"数据双向绑定"
+        }
+    }
+})
+</script>
+<template>
+  <p v-text="message1"></p>
+  <p v-html="message2"></p>
+  <p>{{message3}}</p>
+  <input v-model="message4"> <br>
+  {{message4}}
+</template>
+<style>
+body{
+    text-align: center;
+}
+p{
+    display: block;
+}
+</style>
---
+++ Code/模板语法.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default ({
+    data(){
+        return{
+            message1:"模板语法",
+            message2:"数据只渲染一次",
+            message3:"<span style='color: blueviolet'>输出html语法</span>",
+            message4:"输出纯文本语法",
+            message5:"color:red",//绑定属性
+            message6:"数据双向绑定"
+        }
+    }
+})
+</script>
+<template>
+  {{message1}}
+  <p v-once>{{message2}}</p>
+  <p v-html="message3"></p>
+  <p v-text="message4"></p>
+  <p v-bind:style="message5">绑定color属性</p>
+  <input v-model="message6"> <br>
+  {{message6}}
+</template>
+<style>
+
+</style>
